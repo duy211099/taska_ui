bbc7eab7a90b90b83e0fb738afa61c6d28f6e841
duy <duy.phambaxuan@haravan.com>
2022-05-14 00:44:34 +0700

    import fonts

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
+          fontFamily: 'Urbanist',
           primarySwatch: Colors.blue,
         ),
         home: const Scaffold(
--- lib/src/shared/styles.dart
+++ lib/src/shared/styles.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: public_member_api_docs
-
 import 'package:flutter/material.dart';
+import 'package:taska_ui/taska_ui.dart';
 
 // Text Styles
 
@@ -10,54 +9,65 @@ import 'package:flutter/material.dart';
 const TextStyle heading1Style = TextStyle(
   fontSize: 48,
   fontWeight: FontWeight.bold,
+  fontFamily: urbanist,
 );
 
 const TextStyle heading2Style = TextStyle(
   fontSize: 40,
   fontWeight: FontWeight.bold,
+  fontFamily: urbanist,
 );
 
 const TextStyle heading3Style = TextStyle(
   fontSize: 32,
   fontWeight: FontWeight.bold,
+  fontFamily: urbanist,
 );
 
 const TextStyle heading4Style = TextStyle(
   fontSize: 24,
   fontWeight: FontWeight.bold,
+  fontFamily: urbanist,
 );
 
 const TextStyle heading5Style = TextStyle(
   fontSize: 20,
   fontWeight: FontWeight.bold,
+  fontFamily: urbanist,
 );
 
 const TextStyle heading6Style = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.bold,
+  fontFamily: urbanist,
 );
 
 const TextStyle bodyXLargeStyle = TextStyle(
   fontSize: 18,
   fontWeight: FontWeight.w400,
+  fontFamily: urbanist,
 );
 
 const TextStyle bodyLargeStyle = TextStyle(
   fontSize: 16,
   fontWeight: FontWeight.w400,
+  fontFamily: urbanist,
 );
 
 const TextStyle bodyMediumStyle = TextStyle(
-  fontSize: 14,
+  fontSize: 30,
   fontWeight: FontWeight.w400,
+  fontFamily: urbanist,
 );
 
 const TextStyle bodySmallStyle = TextStyle(
   fontSize: 12,
   fontWeight: FontWeight.w400,
+  fontFamily: urbanist,
 );
 
 const TextStyle bodyXSmallStyle = TextStyle(
   fontSize: 10,
   fontWeight: FontWeight.w400,
+  fontFamily: urbanist,
 );
